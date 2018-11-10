e49da782fffd35737a33ca8787894e870bd5569c
LucasAlva <lucasganoza@gmail.com>
2018-11-10 17:53:49 -0500

    First Commit

---
+++ localization/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/gigas/workspace/localization/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/gigas/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/gigas/catkin_ws/devel;/opt/ros/kinetic" != "" else []
